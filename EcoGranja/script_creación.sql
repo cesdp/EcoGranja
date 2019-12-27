ce48a731a27eeeee4222492b35ae1cb4d3e34728
César Díaz <58976466+cesdp@users.noreply.github.com>
2019-12-27 01:06:01 +0100

    Add files via upload

---
+++ EcoGranja/script_creación.sql
@@ -0,0 +1,245 @@
+﻿create sequence seq_numeroAdmin;
+create sequence seq_numeroPedido;
+create sequence seq_numeroSocio;
+create sequence seq_numeroTarjeta;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+create table UsuarioAdministrador(
+	nombreCuenta varchar(20),
+	clave varchar(255) not null,
+	email varchar(50) not null unique,
+	imagenUsuario bytea,
+	direccion varchar(40) not null,
+	numeroAdmin integer default nextval('seq_numeroAdmin') not null unique,
+	primary key(nombreCuenta)
+);
+
+create table UsuarioClienteProductorComprador(
+	nombreCuenta varchar(20),
+	clave varchar(255) not null,
+	email varchar(50) not null unique,
+	imagenUsuario bytea,
+	direccion varchar(40) not null,
+	numeroSocio integer default nextval('seq_numeroSocio') not null unique,
+	nombrePropio varchar(20) ,
+	apellidos varchar(40),
+	dni varchar(9),
+	numeroCuenta varchar(24),
+	direccionEnvio varchar(40),
+	localidad varchar(20),
+	provincia varchar(20),
+	pais varchar(20),
+	telefonoContacto varchar(11),
+	saldoPropio float DEFAULT 0,
+	esProductor boolean not null,
+	esConsumidor boolean not null,
+	primary key(nombreCuenta)
+);
+
+create table Granja(
+	nombre varchar(20) not null,
+	cif varchar(9),
+	provincia varchar(20) not null,
+	pais varchar(20) not null,
+	direccion varchar(40) not null,
+	tipo varchar(15) not null,
+	productor varchar(20) not null,
+	primary key(cif),
+	foreign key(productor) references UsuarioClienteProductorComprador(nombreCuenta) on delete restrict on update cascade
+);
+
+
+create table Producto(
+	nombre varchar(20),
+	granja varchar(9),
+	categoria varchar(20) not null,
+	descripcion varchar(750) not null,
+	imagen bytea,
+	coste float not null,
+	primary key(nombre,granja),
+	foreign key(granja) references Granja(cif) on delete cascade on update cascade
+);
+
+
+
+create table Lote(
+	codigoBarras bigint,
+	unidades integer not null,
+	fechaCaducidad date not null,
+	producto varchar(20),
+	granja varchar(9),
+	primary key(codigoBarras, producto,granja),
+	foreign key(producto,granja) references Producto(nombre,granja) on delete cascade on update cascade
+);
+
+create table MetododePago(
+	id integer not null ,
+	comprador varchar(20),
+	primary key(id, comprador),
+	foreign key(comprador) references UsuarioClienteProductorComprador(nombreCuenta) on delete cascade on update cascade
+);
+
+create table Tarjeta(
+	id integer  not null ,
+	numero varchar(12) ,
+	comprador varchar(20),
+	caducidad varchar(4) not null,
+	cvv varchar(3) not null,
+	primary key(id, comprador),
+	foreign key(id, comprador) references MetododePago(id, comprador) on delete cascade on update cascade
+);
+
+create table CuentaBancaria(
+	id integer  not null,
+	numeroCuenta varchar(24),
+	comprador varchar(20),
+	primary key(id, numeroCuenta),
+	foreign key(id, comprador) references MetododePago(id, comprador) on delete cascade on update cascade
+);
+
+create table Pedido(
+	id integer not null ,
+	comprador varchar(20),
+	usaSaldoPropio boolean not null,
+	fechaCompra timestamp not null,
+	total float not null,
+	metodoPago integer not null ,
+	primary key(id, comprador),
+	foreign key(metodoPago, comprador) references MetododePago(id, comprador) on delete cascade on update cascade
+);
+
+create table incluye(
+	unidadesCompr integer,
+	devolucion boolean not null,
+	comprador varchar(20),
+	administrador varchar(20),
+	lote bigint,
+	producto varchar(20),
+	granja varchar(9),
+	pedido integer not null,
+	fechaDevolucion timestamp,
+	fechaProcesado timestamp,
+	unidadesDev integer,
+	primary key(unidadesCompr, comprador, lote, producto,pedido,granja),
+	foreign key(lote, producto,granja) references Lote(codigoBarras, producto,granja) on delete cascade on update cascade,
+	foreign key(pedido, comprador) references Pedido(id, comprador) on delete cascade on update cascade,
+	foreign key(administrador) references UsuarioAdministrador(nombreCuenta) on delete cascade on update cascade
+);
+
+
+--Triggers
+
+create or replace function controla_secuencias_metodos() returns trigger as $csl$
+begin
+  case TG_OP
+    when 'INSERT' then
+      execute 'create sequence seq_metodos_comprador_'||(NEW.nombreCuenta);
+      return new;
+    when 'UPDATE' then
+      execute 'drop sequence seq_metodos_comprador_'||(OLD.nombreCuenta );
+      execute 'create sequence seq_metodos_comprador_'||(NEW.nombreCuenta );
+      return new;
+    when 'DELETE' then 
+      execute 'drop sequence seq_metodos_comprador_'||(OLD.nombreCuenta );
+      return old;
+    else null;
+  end case;
+end;
+$csl$ Language plpgsql;
+
+
+create trigger afterMetodos after insert or update or delete on UsuarioClienteProductorComprador
+for each row execute procedure controla_secuencias_metodos();
+
+create or replace function controla_secuencias_pedidos() returns trigger as $csl$
+begin
+  case TG_OP
+    when 'INSERT' then
+      execute 'create sequence seq_pedidos_comprador_'||(NEW.nombreCuenta );
+      return new;
+    when 'UPDATE' then
+      execute 'drop sequence seq_pedidos_comprador_'||(OLD.nombreCuenta);
+      execute 'create sequence seq_pedidos_comprador_'||(NEW.nombreCuenta);
+      return new;
+    when 'DELETE' then 
+      execute 'drop sequence seq_pedidos_comprador_'||(OLD.nombreCuenta);
+      return old;
+    else null;
+  end case;
+end;
+$csl$ Language plpgsql;
+
+
+create trigger afterPedido after insert or update or delete on UsuarioClienteProductorComprador
+for each row execute procedure controla_secuencias_pedidos();
+
+
+
+
+
+
+create or replace function controla_devolucion() returns trigger as $csl$
+declare
+	precio double precision;
+	devueltas integer;
+	compradas integer;
+begin
+      IF NEW.devolucion=true THEN
+	select sum(unidadesDev) into devueltas from incluye where lote=NEW.lote  and producto=NEW.producto and pedido=NEW.pedido and comprador=NEW.comprador and granja=NEW.granja ;
+	select sum(unidadesCompr) into compradas from incluye where lote=NEW.lote  and producto=NEW.producto and pedido=NEW.pedido and comprador=NEW.comprador and granja=NEW.granja  ;
+		IF devueltas <=compradas THEN
+		     select coste into precio from producto where nombre=NEW.producto and granja=NEW.granja ;
+		     update UsuarioClienteProductorComprador set saldopropio= saldopropio+ NEW.unidadesDev*precio  where nombreCuenta=NEW.comprador;
+		ELSE
+			RAISE EXCEPTION 'Cantidad de devueltas mayor que compradas: % > %', devueltas,compradas 
+ 			  USING HINT = 'Revisa las unidades devueltas';
+		END IF;
+      END IF;
+      return new;
+end;
+$csl$ Language plpgsql;
+
+create trigger gestionaDevolucion after update or insert on incluye
+for each row execute procedure controla_devolucion();
+
+
+create or replace function controla_compra() returns trigger as $csl$
+DECLARE
+    costeT double precision;
+    valorProducto double precision;
+BEGIN
+    select sum(total) into costeT from Pedido where id=NEW.pedido and comprador=NEW.comprador;
+    select sum(coste) into valorProducto from producto where nombre= NEW.producto;
+    costeT:= costeT + NEW.unidadesCompr* valorProducto;
+    UPDATE Pedido SET total=costeT WHERE id=NEW.pedido  and comprador=NEW.comprador;
+    RETURN new;
+END;
+$csl$ Language plpgsql;
+
+create trigger gestionaCompra after insert on incluye
+for each row execute procedure controla_compra();
+
+--Funciones
+create or replace function introduce_num_pedido() returns trigger as $csl$
+begin
+  new.id:=nextval('seq_pedidos_comprador_'||cast(NEW.comprador as text));
+  return new;
+end;
+$csl$ Language plpgsql;
+
+
+create trigger beforeInsertPedidos before insert on Pedido
+for each row execute procedure introduce_num_pedido();
+
+
+create or replace function introduce_num_metodo() returns trigger as $csl$
+begin
+  new.id:=nextval('seq_metodos_comprador_'||cast(NEW.comprador as text));
+  return new;
+end;
+$csl$ Language plpgsql;
+
+
+create trigger beforeInsertMetodos before insert on MetododePago
+for each row execute procedure introduce_num_metodo();
+
---
+++ EcoGranja/script_inserción.sql
@@ -0,0 +1,75 @@
+﻿INSERT INTO UsuarioAdministrador(nombreCuenta,clave,email,direccion) VALUES('CesDP',PGP_SYM_ENCRYPT('12345678','AES_KEY'),'linces@gmail.com','Monte da Condesa');
+INSERT INTO UsuarioAdministrador(nombreCuenta,clave,email,direccion) VALUES('Admin',PGP_SYM_ENCRYPT('admin','AES_KEY'),'administracion@admin.com','Madrid');
+INSERT INTO UsuarioClienteProductorComprador (nombreCuenta,clave,email, direccion,nombrePropio,apellidos,dni,numeroCuenta,direccionEnvio,localidad,provincia,pais,telefonoContacto,esProductor,esConsumidor)
+VALUES ('FerAL',PGP_SYM_ENCRYPT('87654321','AES_KEY'),'fer@rai.usc.es','Ourense','Fernando','Arrojo Alonso','35678965B','ES604060013412340005','Ourense 12','Ourense','Ourense','España','654123890',true,true);
+INSERT INTO UsuarioClienteProductorComprador (nombreCuenta,clave,email, direccion,nombrePropio,apellidos,dni,numeroCuenta,direccionEnvio,localidad,provincia,pais,telefonoContacto,esProductor,esConsumidor)
+VALUES ('DavidLC',PGP_SYM_ENCRYPT('87654321','AES_KEY'),'david@rai.usc.es','Sanxenxo','David','Landin Calvo',NULL,NULL,'Sanxenxo 12','Ourense','Ourense','España','654123890',false,true);
+INSERT INTO UsuarioClienteProductorComprador (nombreCuenta,clave,email, direccion,nombrePropio,apellidos,dni,numeroCuenta,direccionEnvio,localidad,provincia,pais,telefonoContacto,esProductor,esConsumidor)
+VALUES ('JuanDL',PGP_SYM_ENCRYPT('87654321','AES_KEY'),'juan@rai.usc.es','Santiago','Juan','Deus Lorenzo','45678965J','ES754060013512330005',NULL,NULL,NULL,NULL,NULL,true,false);
+INSERT INTO UsuarioClienteProductorComprador (nombreCuenta,clave,email, direccion,nombrePropio,apellidos,dni,numeroCuenta,direccionEnvio,localidad,provincia,pais,telefonoContacto,esProductor,esConsumidor)
+VALUES ('David',PGP_SYM_ENCRYPT('1234','AES_KEY'),'david@comprador.com','Barcelona 12','David','Otero Díaz',NULL,NULL,'Barcelona 12','Madrid','Madrid','España','625127184',false,true);
+
+INSERT INTO Granja VALUES('Hnos Rivera','ES1234567','A Coruña','España','Betanzos','Vacuna','FerAL');
+INSERT INTO Granja VALUES('La Granjita','ES9874123','Lugo','España','Vilalba','Horticola','FerAL');
+INSERT INTO Granja VALUES('Hnos Fernandez','ES1238868','Pontevedra','España','Vigo','Frutal','JuanDL');
+INSERT INTO Granja VALUES('Pollos AR','ES1274123','Ourense','España','Ourense','Avicola','JuanDL');
+
+INSERT INTO Producto VALUES('Aguacate','ES1238868','Fruta','Descripcion de un aguacate',cast('00000' as bytea),2.50);
+INSERT INTO Producto VALUES('Fresa','ES1238868','Fruta','Descripcion de una fresa',cast('00000' as bytea),1.50);
+INSERT INTO Producto VALUES('Pimiento','ES9874123','Verdura','Descripcion de un pimiento',cast('00000' as bytea),0.90);
+INSERT INTO Producto VALUES('Zanahoria','ES9874123','Verdura','Descripcion de una zanahoria',cast('00000' as bytea),1.10);
+INSERT INTO Producto VALUES('Leche','ES1234567','Lácteo','Descripcion de la leche',cast('00000' as bytea),0.60);
+INSERT INTO Producto VALUES('Queso','ES1234567','Lácteo','Descripcion del queso',cast('00000' as bytea),3.50);
+INSERT INTO Producto VALUES('Huevos','ES1274123','Avícola','Descripcion de un huevos',cast('00000' as bytea),0.75);
+
+INSERT INTO Lote VALUES(81234567456789,15,'2019-05-12','Aguacate','ES1238868');
+INSERT INTO Lote VALUES(81234567455555,30,'2019-05-12','Fresa','ES1238868');
+INSERT INTO Lote VALUES(45678695867689,10,'2019-05-12','Pimiento','ES9874123');
+INSERT INTO Lote VALUES(24367580456789,20,'2019-05-30','Zanahoria','ES9874123');
+INSERT INTO Lote VALUES(96947365868464,50,'2019-05-12','Leche','ES1234567');
+INSERT INTO Lote VALUES(46483929467574,25,'2019-07-14','Queso','ES1234567');
+INSERT INTO Lote VALUES(74758484647685,120,'2019-05-20','Huevos','ES1274123');
+
+INSERT INTO MetododePago(comprador) VALUES('FerAL');
+INSERT INTO MetododePago(comprador) VALUES('JuanDL');
+
+INSERT INTO MetododePago(comprador) VALUES('FerAL');
+INSERT INTO Tarjeta(id,numero,comprador,caducidad,cvv) VALUES(1,'543215678665','FerAL','2018','632');
+INSERT INTO MetododePago(comprador) VALUES('DavidLC');
+INSERT INTO CuentaBancaria(id,numeroCuenta,comprador) VALUES(1,'ES508908765431233456','DavidLC');
+INSERT INTO MetododePago(comprador) VALUES('David');
+INSERT INTO CuentaBancaria(id,numeroCuenta,comprador) VALUES(1,'ES508908765431233457','David');
+
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('FerAL',false,0,1,'2019-03-15');
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('DavidLC',false,0,1,'2019-03-25');
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('DavidLC',false,0,1,'2019-03-25');
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('David',false,0,1,'2019-03-23');
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('DavidLC',false,0,1,'2019-03-25');
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('FerAL',false,0,1,'2019-03-15');
+INSERT INTO Pedido(comprador,usaSaldoPropio,total,metodoPago,fechaCompra) VALUES('FerAL',false,0,1,'2019-03-16');
+
+INSERT INTO incluye VALUES(5,false,'FerAL',NULL,81234567456789,'Aguacate','ES1238868',1,NULL,NULL,0);
+INSERT INTO incluye VALUES(6,false,'FerAL',NULL,24367580456789,'Zanahoria','ES9874123',1,NULL,NULL,0);
+INSERT INTO incluye VALUES(2,false,'FerAL',NULL,46483929467574,'Queso','ES1234567',1,NULL,NULL,0);
+INSERT INTO incluye VALUES(1,false,'FerAL',NULL,74758484647685,'Huevos','ES1274123',1,NULL,NULL,0);
+
+INSERT INTO incluye VALUES(5,false,'DavidLC',NULL,81234567456789,'Aguacate','ES1238868',1,NULL,NULL,0);
+INSERT INTO incluye VALUES(2,false,'DavidLC',NULL,81234567455555,'Fresa','ES1238868',1,NULL,NULL,0);
+INSERT INTO incluye VALUES(1,false,'DavidLC','CesDP',96947365868464,'Leche','ES1234567',1,'2019-04-12','2019-04-14',1);
+INSERT INTO incluye VALUES(6,false,'DavidLC',NULL,74758484647685,'Huevos','ES1274123',1,NULL,NULL,0);
+
+INSERT INTO incluye VALUES(5,false,'DavidLC',NULL,81234567456789,'Aguacate','ES1238868',2,NULL,NULL,0);
+INSERT INTO incluye VALUES(10,false,'DavidLC',NULL,81234567455555,'Fresa','ES1238868',2,NULL,NULL,0);
+INSERT INTO incluye VALUES(6,false,'DavidLC','Admin',74758484647685,'Huevos','ES1274123',2,'2019-04-12','2019-04-14',3);
+
+INSERT INTO incluye VALUES(20,true,'David','Admin',46483929467574,'Queso','ES1234567',1,'2019-03-30','2019-04-01',5);
+INSERT INTO incluye VALUES(30,false,'David',NULL,74758484647685,'Huevos','ES1274123',1,'2019-03-30',NULL,15);
+INSERT INTO incluye VALUES(5,true,'David','Admin',96947365868464,'Leche','ES1234567',1,'2019-03-30','2019-04-01',5);
+
+INSERT INTO incluye VALUES(30,false,'DavidLC',NULL,74758484647685,'Huevos','ES1274123',3,NULL,NULL,0);
+
+INSERT INTO incluye VALUES(20,false,'FerAL',NULL,46483929467574,'Queso','ES1234567',2,NULL,NULL,0);
+INSERT INTO incluye VALUES(30,false,'FerAL',NULL,74758484647685,'Huevos','ES1274123',2,NULL,NULL,0);
+
+INSERT INTO incluye VALUES(10,false,'FerAL',NULL,96947365868464,'Leche','ES1234567',3,'2019-04-12',NULL,5);
+INSERT INTO incluye VALUES(5,false,'FerAL',NULL,74758484647685,'Huevos','ES1274123',3,'2019-04-12',NULL,3);
